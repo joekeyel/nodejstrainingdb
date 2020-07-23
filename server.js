60f2faf539579a9ee090adddcb2ffb92c443a0d0
Hasanul Isyraf <hasanul2307@gmail.com>
2020-07-23 11:36:58 +0800

    add for training connect 2 db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan  = require('morgan')
-
+const Promise = require("bluebird");
 const app = express();
 app.use(morgan('combined'))
 
@@ -45,9 +45,7 @@ tunnel(config, function (error, server) {
     console.log(error.message);
   }
 
-  console.log("tunnel connected")
-
-
+  console.log("tunnel jumphost connected")
 
   //....
 });
@@ -104,43 +102,267 @@ const callapi = app.get('/api/location',(req,res)=>{
 
 const callapi2 = app.get('/api/user',(req,res)=>{
 
-  connection.execute(
-    `SELECT *
-     FROM DC_USER`,
-    [],  
-   function(err, result) {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      console.log(result.rows);
-      res.send({"users":result.rows});
-   });
+  userId = req.query.userid
+      
+    
 
+  if (userId != null) {
+    query1=" where dcu_id='"+userId+"'";
+    }else{
+      query1="";
+    };
+    
+    if (userId != null) {
+    query2=" and rl_dcu_id='"+userId+"'";
+    }else{
+      query2="";
+    };
+    
+    if (userId != null) {
+    query3=" and ur_dcu_id= '"+userId+"'";
+    }else{
+      query3="";
+    };
+      
+    if (userId != null) {
+    query4=" and uw_dcu_id= '"+userId+"'";
+    }else{
+      query4="";
+    };
+
+
+
+  async function makeConnection() {
+    try {
+      connection = await oracledb.getConnection({
+          user: "TMIMS",
+          password: password,
+          connectString: "127.0.0.1:1521/BQMDEV"
+      });
+      console.log('connected to database');
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      if (connection) {
+        // try {
+        //   // Always close connections
+        //   await connection.close(); 
+        //   console.log('close connection success');
+        // } catch (err) {
+        //   console.error(err.message);
+        // }
   
+        // connection.execute(
+        //   `SELECT *
+        //    FROM DC_USER`,
+        //   [],  
+        //  function(err, result) {
+        //     if (err) {
+        //       console.error(err.message);
+        //       return;
+        //     }
+        //     console.log(result.rows);
+        //     res.send(result.rows)
+        //  });
+
+
+
+        var query1p =  connection.execute(
+
+          `select t.dcu_id as id,t.dcu_name as name,t.dcu_email as email,t.dcu_admin as user_admin,t.dcu_approve as user_approve,t.dcu_edit as user_edit,
+          t.dcu_desc,t.dcu_notification,
+          (select 
+          LISTAGG('"'||WG_NAME||'"', ' , ') WITHIN GROUP (ORDER BY WG_NAME)
+          from (SELECT  UW.UW_DCU_ID,W.WG_NAME FROM DC_USER_WORKGROUP UW, dc_workgroup W
+          where uw.uw_wg_id=w.wg_id) where UW_DCU_ID=t.dcu_id) as workgroup
+          from DC_USERS t `+ query1,
+          [],  
+         
+          );
+
+          var query2p = connection.execute(
+          `SELECT b.ro_name as roles FROM DC_USER_ROLE A, DC_ROLES B
+          where a.rl_ro_id=b.ro_id `+ query2,
+          []); 
+
+          var query3p = connection.execute(
+            `SELECT  r.ur_dcu_id,c.rt_name as rights FROM DC_USER_rights r, dc_rights c
+            where r.ur_rt_id=c.rt_id  `+ query3,
+            []); 
+
+            var query4p = connection.execute(
+              `SELECT  C.WG_NAME as workgroups FROM DC_USER_workgroup r, dc_workgroup c
+              where r.uw_wg_id=c.wg_id   `+ query4,
+              []); 
+
+          Promise.join(query1p, query2p, query3p, query4p).spread(function (result, result2, result3, result4){
+
+            var rolesarray = [];
+            var rightsarray = [];
+            var workgrouparray = [];
+              
+            result2.rows.forEach(element => {
+              rolesarray.push(element.ROLES)
+            });
+
+            result3.rows.forEach(element => {
+              rightsarray.push(element.RIGHTS)
+            });
+
+            result4.rows.forEach(element => {
+              workgrouparray.push(element.WORKGROUPS)
+            });
+
+
+            res.status(200).send( { user: result.rows, roles: rolesarray , rights: rightsarray, workgroups: workgrouparray});
+           // return connection.close();
+          })
+          .catch(function (err){
+            console.log(err.message);
+            //return connection.close();
+          });
+    
+      }
+    }
+  
+  
+  }
+
+  makeConnection()
+
 })
 
 
-const callapi3 = app.get('/api/DC_NE_UTILIZATION/:siteName',(req,res)=>{
+// to connect 2 db and combine query to get one json response
 
-  connection.execute(
-    `select (select count(*) from dc_rack d2 where d2.rack_site_id=d.rack_site_id and d2.rack_status='Unoccupied' group by rack_site_id)rack_utilized,
-    (select count(*) from dc_rack d3 where d3.rack_site_id=d.rack_site_id and d3.rack_status='Registered' group by rack_site_id)rack_available,
-    count(*) as total_rack,
-    (select round(count(*)*100 / (select count(*) from dc_rack d4 where d4.rack_site_id=d.rack_site_id group by rack_site_id)) from dc_rack d2 where d2.rack_site_id=d.rack_site_id and d2.rack_status='Unoccupied' group by rack_site_id)rack_utilization
-    from dc_rack d
-    where d.rack_site_id=(select S.SITE_ID from dc_site s where s.site_name =:siteName)
-    group by rack_site_id`,
-    [req.params.siteName],  
-   function(err, result) {
-      if (err) {
-        console.error(err.message);
-        return;
+const callapi3 = app.get('/api/connect2db',(req,res)=>{
+
+  userId = req.query.userid
+      
+    
+
+  if (userId != null) {
+    query1=" where dcu_id='"+userId+"'";
+    }else{
+      query1="";
+    };
+    
+    if (userId != null) {
+    query2=" and rl_dcu_id='"+userId+"'";
+    }else{
+      query2="";
+    };
+    
+    if (userId != null) {
+    query3=" and ur_dcu_id= '"+userId+"'";
+    }else{
+      query3="";
+    };
+      
+    if (userId != null) {
+    query4=" and uw_dcu_id= '"+userId+"'";
+    }else{
+      query4="";
+    };
+
+
+  var nicedb = false
+  var granitedb = false
+  //to connect 2 database here
+
+  async function makeConnectionNiceDB() {
+    try {
+      connection = await oracledb.getConnection({
+          user: "TMIMS",
+          password: password,
+          connectString: "127.0.0.1:1521/BQMDEV"
+      });
+      console.log('connected to database');
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      if (connection) {
+
+        console.log("Connected to niceDB")
+        nicedb = true
+
       }
-      console.log(result.rows);
-      res.send({"sitename":result.rows});
-   });
 
+    }
+
+  }
+
+  makeConnectionNiceDB()
+
+  async function makeConnectionGraniteDB() {
+    try {
+      connection2 = await oracledb.getConnection({
+          user: "TMIMS",
+          password: password,
+          connectString: "127.0.0.1:1521/BQMDEV"
+      });
+      console.log('connected to database');
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      if (connection2) {
+
+        console.log("Connected to graniteDB")
+        granitedb = true
+
+        console.log("start query here");
+
+
+        var query1p =  connection2.execute(
+    
+          `select t.dcu_id as id,t.dcu_name as name,t.dcu_email as email,t.dcu_admin as user_admin,t.dcu_approve as user_approve,t.dcu_edit as user_edit,
+          t.dcu_desc,t.dcu_notification,
+          (select 
+          LISTAGG('"'||WG_NAME||'"', ' , ') WITHIN GROUP (ORDER BY WG_NAME)
+          from (SELECT  UW.UW_DCU_ID,W.WG_NAME FROM DC_USER_WORKGROUP UW, dc_workgroup W
+          where uw.uw_wg_id=w.wg_id) where UW_DCU_ID=t.dcu_id) as workgroup
+          from DC_USERS t `+ query1,
+          [],  
+         
+          );
+    
+          var query2p = connection.execute(
+          `SELECT b.ro_name as roles FROM DC_USER_ROLE A, DC_ROLES B
+          where a.rl_ro_id=b.ro_id `+ query2,
+          []); 
+    
+    
+          Promise.join(query1p, query2p).spread(function (result, result2){
+    
+            var rolesarray = [];
+            var rightsarray = [];
+            var workgrouparray = [];
+              
+            result2.rows.forEach(element => {
+              rolesarray.push(element.ROLES)
+            });
+    
+      
+    
+            res.status(200).send( { user: result.rows, roles: rolesarray});
+            //return connection.close();
+          })
+          .catch(function (err){
+            console.log(err.message);
+            //return connection.close();
+          });
+    
+
+      }
+
+    }
+
+  }
+
+  
+  makeConnectionGraniteDB()
+
+  
   
 })
 
